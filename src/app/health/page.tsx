632050359fc5090f7ad8033f60d34ef5fc23b968
Alex Chen <112523chen@gmail.com>
2025-01-02 15:45:18 -0500

    chore(): update HC endpoint

--- src/app/health/page.tsx
+++ src/app/health/page.tsx
@@ -1,10 +1,7 @@
 import type { FC } from "react";
 
 const HealthPage: FC = () => {
-  // check if browser or server has requested the page
-  const isBrowser = typeof window !== "undefined";
-
-  return <div>200 OK {isBrowser ? "Client" : "Server"}</div>;
+  return <div>200 OK</div>;
 };
 
 export default HealthPage;
