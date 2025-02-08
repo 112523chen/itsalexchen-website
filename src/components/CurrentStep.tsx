a87c8d74d6d84515310eed38198c2d43985e610b
Alex Chen <112523chen@gmail.com>
2025-02-08 09:49:59 -0500

    chore(): update content for Feb 25

--- src/components/CurrentStep.tsx
+++ src/components/CurrentStep.tsx
@@ -7,12 +7,12 @@ const CurrentStep: FC = () => {
         <h3 className="mb-3 flex items-center text-2xl font-bold">
           <span className="text-accent mr-2">*</span> what i&apos;m working on
         </h3>
-        <p className="text-gray-500">Last updated on 2025-01-01</p>
+        <p className="text-gray-500">Last updated on 2025-02-08</p>
       </div>
       <p>
-        Currently I&apos;m continuing to develop my knowledge and skills in
-        Azure cloud services and how to build scalable and cost-effective data
-        solutions multi-cloud environments. I&apos;m also working on a few side
+        Currently I&apos;m continuing to develop my knowledge and
+        skills in building scalable and cost-effective data
+        solutions in both hybrid-cloud and multi-cloud environments. I&apos;m also working on a few side
         projects that I&apos;m excited to share soon!
       </p>
     </section>
